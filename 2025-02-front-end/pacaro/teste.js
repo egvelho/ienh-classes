a4300e727c65f4aeb7668d4157ad3008890d74bb
Eduarda Velho <email@eduardavelho.com>
2025-11-05 20:16:30 -0300

    up

--- 2025-02-front-end/pacaro/teste.js
+++ 2025-02-front-end/pacaro/teste.js
@@ -1,1 +1,19 @@
 console.log("hello");
+
+const nums = [1, 2, 3, 4, 5];
+const dobros = nums.map((num) => num * 2);
+const dobros2 = nums.map(function (num) {
+  return num * 2;
+});
+const dobros3 = nums.map((num) => {
+  return num * 2;
+});
+
+function dobra(num) {
+  return num * 2;
+}
+const dobros4 = nums.map(dobra);
+
+const letras = ["a", "b", "c", "d", "e"];
+
+//'a'.toUpperCase();
