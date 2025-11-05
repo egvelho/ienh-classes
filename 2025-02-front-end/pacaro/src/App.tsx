bd58efa5f55e0311974675946fc8b2283a031507
Eduarda Velho <email@eduardavelho.com>
2025-11-05 19:18:21 -0300

    up

--- 2025-02-front-end/pacaro/src/App.tsx
+++ 2025-02-front-end/pacaro/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
   function quandoInicia() {
@@ -7,5 +7,43 @@ export default function App() {
 
   useEffect(quandoInicia, []);
 
-  return <div className="bg-blue-800">hello</div>;
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  function incrementaContador() {
+    setCounter(counter + 1);
+  }
+
+  function decrementaContador() {
+    setCounter(counter - 1);
+  }
+
+  return (
+    <div className="p-4">
+      <button
+        onClick={decrementaContador}
+        className="bg-orange-600 p-2 text-white border border-gray-700"
+      >
+        -
+      </button>
+      <input
+        type="text"
+        value={counter}
+        className="border border-gray-700 p-2"
+      />
+      <button
+        onClick={incrementaContador}
+        className="bg-orange-600 p-2 text-white border border-gray-700"
+      >
+        +
+      </button>
+    </div>
+  );
 }
