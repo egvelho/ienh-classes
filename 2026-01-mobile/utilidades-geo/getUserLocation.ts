43f1e4ebc673f7a3f5d2e2d8b5fe86ebb69257d1
Eduarda Velho <email@eduardavelho.com>
2026-04-15 18:25:11 -0300

    up

---
+++ 2026-01-mobile/utilidades-geo/getUserLocation.ts
@@ -0,0 +1,11 @@
+import * as Location from "expo-location";
+
+export async function getUserLocation() {
+  let { status } = await Location.requestForegroundPermissionsAsync();
+  if (status !== "granted") {
+    return null;
+  }
+
+  let location = await Location.getCurrentPositionAsync({});
+  return location;
+}
