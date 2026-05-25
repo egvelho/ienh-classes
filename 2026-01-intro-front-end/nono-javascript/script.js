80bb494fa1a663345fbd5fdddcaa92753ca8fb7a
Eduarda Velho <email@eduardavelho.com>
2026-05-25 20:10:08 -0300

    up

---
+++ 2026-01-intro-front-end/nono-javascript/script.js
@@ -0,0 +1,80 @@
+const pessoas = [
+  {
+    nome: "Eduarda",
+    sobrenome: "Velho",
+    idade: 30,
+  },
+  {
+    nome: "Vinícius",
+    sobrenome: "Ferreira",
+    idade: 18 + 2,
+    mostraIdade() {
+      alert(this.nome + " " + this.sobrenome);
+    },
+  },
+  {
+    nome: "Nikolas",
+    sobrenome: "Lopes",
+    idade: 19,
+  },
+];
+
+const duda = {
+  nome: "Eduarda",
+  sobrenome: "Velho",
+  idade: 30,
+};
+
+const { nome, sobrenome } = duda;
+//const nome = duda.nome;
+//const sobrenome = duda.sobrenome;
+
+const cores = ["vermelho", "verde", "azul", "amarelo", "roxo"];
+const [primeiraCor, segundaCor, ...coresRestantes] = cores;
+//const { 2: primeiraCor } = cores;
+alert(coresRestantes);
+
+const pessoaSecreta = {
+  nome: "Rodolfo",
+  sobrenome: "Josefino",
+  idade: 67,
+  profissao: "Esmirilhador (com ênfase em farmar aura)",
+  genero: "Helicóptero de Ataque",
+  signo: "Sigma",
+  idolo: "Super Choque",
+  artistaFavorita: "Caneta Azul",
+  filmeFavorito: "Internet - O Filme",
+};
+
+const { artistaFavorita, filmeFavorito, ...informacoesRestantes } =
+  pessoaSecreta;
+
+console.log(informacoesRestantes);
+
+const numerosA = [6, 7, 8];
+const numerosB = [4, 2, 1];
+
+const todosOsNumeros = [...numerosA, ...numerosB];
+
+const pessoaBase = {
+  nome: "Goku",
+  sobrenome: "Vegeta",
+};
+
+const pessoaExtra = {
+  signo: "Serpentário",
+  idade: null,
+};
+
+const pessoaCompleta = {
+  ...pessoaBase,
+  ...pessoaExtra,
+};
+
+console.log(pessoaCompleta);
+
+//mostraNomeCompleto(nome, sobrenome);
+
+function mostraNomeCompleto(nome, sobrenome) {
+  alert(nome + " " + sobrenome);
+}
